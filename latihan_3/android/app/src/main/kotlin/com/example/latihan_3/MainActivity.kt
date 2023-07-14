b8d468ee845b482d7c09420ed3a3d56769a964ee
Noobsmaster699 <129032635+NoobsMaster699@users.noreply.github.com>
2023-07-14 12:39:37 +0700

    Delete latihan_3 directory

--- latihan_3/android/app/src/main/kotlin/com/example/latihan_3/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.latihan_3
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- latihan_3/lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:latihan_3/routes.dart';
-
-void main() {
-  runApp(MaterialApp(
-    onGenerateRoute: RouteGenerator.generateRoute,
-  ));
-}
